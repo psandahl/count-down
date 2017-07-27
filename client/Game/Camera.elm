7d1272ae05a327ad4dd6800e34b4fe98fb8f1e94
Patrik Sandahl <patrik.sandahl@gmail.com>
2017-07-27 14:24:40 +0200

    Pitch and yaw the camera.

--- client/Game/Camera.elm
+++ client/Game/Camera.elm
@@ -1,49 +1,117 @@
 module Game.Camera exposing (Camera, init, mouseMoved)
 
 import Math.Vector3 exposing (Vec3, vec3)
+import Math.Vector3 as Vec
 import Math.Matrix4 exposing (Mat4)
 import Math.Matrix4 as Mat
 import Mouse exposing (Position)
 
 
+{-| The Camera.
+-}
 type alias Camera =
-    { position : Vec3
+    { settings : Settings
     , vMatrix : Mat4
     }
 
 
+{-| The settings that make up the exact camera position.
+-}
+type alias Settings =
+    { -- The unit base vector for camera positioning. Shall always be up.
+      baseVector : Vec3
+
+    -- The magnitude of the base vector, i.e. the camera's distance.
+    , magnitude : Float
+
+    -- The yaw (y) rotate. Angle in degrees.
+    , yaw : Float
+
+    -- The pitch (x) rotate. Angle in degrees.
+    , pitch : Float
+    }
+
+
+{-| Initialize the camera.
+-}
 init : Camera
 init =
     let
-        initial =
-            vec3 0 5 20
-
-        matrix =
-            Mat.makeLookAt initial origo up
+        settings =
+            { baseVector = up
+            , magnitude = 20
+            , yaw = 0
+            , pitch = clampPitch 65
+            }
     in
-        { position = initial
-        , vMatrix = matrix
+        { settings = settings
+        , vMatrix = makeMatrix settings
         }
 
 
+{-| Handle a mouse move event. Repositioning the camera.
+-}
 mouseMoved : Position -> Position -> Camera -> Camera
 mouseMoved from to camera =
     let
-        xDelta =
+        settings =
+            camera.settings
+
+        deltaX =
             to.x - from.x
 
-        rotPosition =
-            Mat.transform (cameraRotateMatrix xDelta) camera.position
+        deltaY =
+            to.y - from.y
+
+        yaw =
+            settings.yaw + (toFloat -deltaX / rotationSpeed * 360)
+
+        pitch =
+            clampPitch <| settings.pitch + (toFloat -deltaY / rotationSpeed * 360)
+
+        newSettings =
+            { settings
+                | yaw = yaw
+                , pitch = pitch
+            }
     in
         { camera
-            | position = rotPosition
-            , vMatrix = Mat.makeLookAt rotPosition origo up
+            | settings = newSettings
+            , vMatrix = makeMatrix newSettings
         }
 
 
-cameraRotateMatrix : Int -> Mat4
-cameraRotateMatrix xDelta =
-    Mat.makeRotate (degrees <| toFloat -xDelta / 800 * 360) up
+{-| The speed of rotation. The mouse must move 1000 pixels for a rotation of
+360 degrees.
+-}
+rotationSpeed : Float
+rotationSpeed =
+    1000
+
+
+{-| Clamp the pitch angle. The camera must never be under the surface.
+-}
+clampPitch : Float -> Float
+clampPitch =
+    clamp 10 80
+
+
+makeMatrix : Settings -> Mat4
+makeMatrix settings =
+    let
+        scaledVector =
+            Vec.scale settings.magnitude settings.baseVector
+
+        pitchMatrix =
+            Mat.makeRotate (degrees settings.pitch) (vec3 1 0 0)
+
+        yawMatrix =
+            Mat.makeRotate (degrees settings.yaw) (vec3 0 1 0)
+
+        eye =
+            Mat.transform (Mat.mul yawMatrix pitchMatrix) scaledVector
+    in
+        Mat.makeLookAt eye origo up
 
 
 origo : Vec3
