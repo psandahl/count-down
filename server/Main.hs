2b58a1654a9337b607869abdf92a21950f0fea2f
Patrik Sandahl <patrik.sandahl@gmail.com>
2017-08-04 23:08:50 +0200

    Simple server. Cannot longer use elm-reactor.

--- server/Main.hs
+++ server/Main.hs
@@ -1,15 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
+import           System.EasyFile ((</>))
 import           Web.Scotty
 
 main :: IO ()
 main =
     scotty 8000 $ do
+
+        -- Routes to the start page.
         get "/index.html" startPage
         get "/" $ redirect "/index.html"
+
+        -- Serving local JavaScript.
+        get "/js/:file" $ scriptFile =<< param "file"
+
+        -- Serving (PNG) texture files.
+        get "/texture/:file" $ textureFile =<< param "file"
 
 startPage :: ActionM ()
 startPage = do
     setHeader "Content-Type" "text/html; charset=utf-8"
-    file "site/index.html"
+    file $ "site" </> "index.html"
+
+scriptFile :: FilePath -> ActionM ()
+scriptFile script = do
+    setHeader "Content-Type" "application/javascript"
+    file $ "site" </> "js" </> script
+
+textureFile :: FilePath -> ActionM ()
+textureFile texture = do
+    setHeader "Content-Type" "image/png"
+    file $ "site" </> "texture" </> texture
